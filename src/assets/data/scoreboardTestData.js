0c820063a07bed7365e75be4b365db75bae9d0a6
KDhieb <khaliddhieb@gmail.com>
2023-06-06 14:49:22 -0700

    add music player component to scoreboard

--- src/assets/data/scoreboardTestData.js
+++ src/assets/data/scoreboardTestData.js
@@ -1,32 +1,46 @@
 export const scoreboardTestData = [
-    {
-        name: "Billie Jean",
-        artist: "Michael Jackson",
-        albumCoverImg: "https://i.discogs.com/KRPG8mda9mjOMCY2dFCSJa2cNcE1YnO5cNjYViUB0kQ/rs:fit/g:sm/q:90/h:500/w:500/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTIxNjk1/Ni0xMjMxNTg3OTY1/LmpwZWc.jpeg",
-        likes: 10
-    },
-    {
-        name: "Space Song",
-        artist: "Beach House",
-        albumCoverImg: "https://i.discogs.com/x-2eIb2Z-zdFTfkwBSraCPXCqzjMFYrYTGaM1qbcDhI/rs:fit/g:sm/q:90/h:593/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTczOTkw/MzctMTUyMDc3ODk0/OS0yNjkxLmpwZWc.jpeg",
-        likes: 20
-    },
-    {
-        name: "Last Summer Whisper",
-        artist: "Anri",
-        albumCoverImg: "https://cdn.albumoftheyear.org/album/130785-timely.jpg",
-        likes: 30
-    },
-    {
-        name: "Bohemian Rhapsody",
-        artist: "Queen",
-        albumCoverImg: "https://i1.sndcdn.com/artworks-000116795481-6fmihq-t500x500.jpg",
-        likes: 40
-    },
-    {
-        name: "Two Weeks",
-        artist: "Grizzly Bear",
-        albumCoverImg: "https://e.snmc.io/i/1200/s/984844a3942e92361958555ac99dc759/2642329.png",
-        likes: 50
-    },
-]
+  {
+    name: "Billie Jean",
+    artist: "Michael Jackson",
+    albumCoverImg:
+      "https://i.discogs.com/KRPG8mda9mjOMCY2dFCSJa2cNcE1YnO5cNjYViUB0kQ/rs:fit/g:sm/q:90/h:500/w:500/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTIxNjk1/Ni0xMjMxNTg3OTY1/LmpwZWc.jpeg",
+    likes: 10,
+    preview_url:
+      "https://p.scdn.co/mp3-preview/433dd3e00a82d231b060e2c7ab10f29249bf7942?cid=0b297fa8a249464ba34f5861d4140e58",
+  },
+  {
+    name: "Space Song",
+    artist: "Beach House",
+    albumCoverImg:
+      "https://i.discogs.com/x-2eIb2Z-zdFTfkwBSraCPXCqzjMFYrYTGaM1qbcDhI/rs:fit/g:sm/q:90/h:593/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTczOTkw/MzctMTUyMDc3ODk0/OS0yNjkxLmpwZWc.jpeg",
+    likes: 20,
+    preview_url:
+      "https://p.scdn.co/mp3-preview/433dd3e00a82d231b060e2c7ab10f29249bf7942?cid=0b297fa8a249464ba34f5861d4140e58",
+  },
+  {
+    name: "Last Summer Whisper",
+    artist: "Anri",
+    albumCoverImg: "https://cdn.albumoftheyear.org/album/130785-timely.jpg",
+    likes: 30,
+    preview_url:
+      "https://p.scdn.co/mp3-preview/433dd3e00a82d231b060e2c7ab10f29249bf7942?cid=0b297fa8a249464ba34f5861d4140e58",
+  },
+  {
+    name: "Bohemian Rhapsody",
+    artist: "Queen",
+    albumCoverImg:
+      "https://i1.sndcdn.com/artworks-000116795481-6fmihq-t500x500.jpg",
+    likes: 40,
+    preview_url:
+      "https://p.scdn.co/mp3-preview/433dd3e00a82d231b060e2c7ab10f29249bf7942?cid=0b297fa8a249464ba34f5861d4140e58",
+  },
+  {
+    name: "Two Weeks",
+    artist: "Grizzly Bear",
+    albumCoverImg:
+      "https://e.snmc.io/i/1200/s/984844a3942e92361958555ac99dc759/2642329.png",
+    likes: 50,
+    preview_url:
+      "https://p.scdn.co/mp3-preview/433dd3e00a82d231b060e2c7ab10f29249bf7942?cid=0b297fa8a249464ba34f5861d4140e58",
+  },
+];
--- src/components/ScoreboardSongCard.js
+++ src/components/ScoreboardSongCard.js
@@ -1,11 +1,16 @@
 import "../styling/scoreboardSongCard.css";
+import MusicPlayer from "./MusicPlayer";
 
 export const ScoreboardSongCard = ({ song, ranking }) => {
   return (
-    <div className="liked-song-card-container">
+    <div className='liked-song-card-container'>
       <p>{ranking}</p>
-      <div className="song-info">
-        <img src={song.albumCoverImg} />
+      <div className='song-info'>
+        <MusicPlayer
+          url={song.preview_url}
+          img={song.albumCoverImg}
+          mini={false}
+        />
         <p>
           {song.name} - {song.artist}
         </p>
