0573cd0667dcb008411369145404f97b0c97ddef
Parmvir-S <parmvir.shergill@gmail.com>
2023-06-06 14:49:15 -0700

    Made better use of MUI

--- src/components/ScoreboardSongCard.js
+++ src/components/ScoreboardSongCard.js
@@ -1,16 +1,30 @@
-import "../styling/scoreboardSongCard.css";
+import { makeStyles } from "@material-ui/core/styles";
+import { TableCell, TableRow } from "@material-ui/core";
+import { IMAGE_ALTS } from "../constants/constants";
+
+const useStyles = makeStyles({
+  img: {
+    width: 150,
+    height: 150,
+  },
+});
 
 export const ScoreboardSongCard = ({ song, ranking }) => {
+  const classes = useStyles();
   return (
-    <div className="liked-song-card-container">
-      <p>{ranking}</p>
-      <div className="song-info">
-        <img src={song.albumCoverImg} />
-        <p>
-          {song.name} - {song.artist}
-        </p>
-        <p>{song.likes}</p>
-      </div>
-    </div>
+    <TableRow>
+      <TableCell>{ranking}</TableCell>
+      <TableCell>
+        <img
+          className={classes.img}
+          src={song.albumCoverImg}
+          alt={IMAGE_ALTS.albumCover}
+        />
+      </TableCell>
+      <TableCell>
+        {song.name} - {song.artist}
+      </TableCell>
+      <TableCell>{song.likes}</TableCell>
+    </TableRow>
   );
 };
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,5 +1,10 @@
 export const SCOREBOARD_HEADERS = {
+    ranking: "Ranking",
     album: "Album",
     nameArtist: "Name - Artist",
     likes: "Likes"
+}
+
+export const IMAGE_ALTS = {
+    albumCover: "Album Cover"
 }
