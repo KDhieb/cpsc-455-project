e8b9967238e3cae063ad2817a1c50fa3920e62f3
KDhieb <khaliddhieb@gmail.com>
2023-06-08 17:32:34 -0700

    implemented dynamic resizing for carousel, added useWindowDimensions hook

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux';
-import store from './store';
-import {ThemeProvider} from "@mui/material/styles";
-import {theme} from "./styling/theme"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import store from "./store";
+import { ThemeProvider } from "@mui/material/styles";
+import { darkTheme } from "./styling/theme";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-        <ThemeProvider theme={theme}>
-            <App/>
-        </ThemeProvider>
+      <ThemeProvider theme={darkTheme}>
+        <App />
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>
 );
