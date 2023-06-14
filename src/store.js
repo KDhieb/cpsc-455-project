f6a9ea43f61ae1ffbd2c0225c57b285d0ac3ee12
KDhieb <khaliddhieb@gmail.com>
2023-06-13 21:06:37 -0400

    remove imports

--- src/store.js
+++ src/store.js
@@ -1,13 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import songSearchReducer from "./slices/songSearchSlice";
-import recommendedSongsReducer from "./slices/recommendedSongsSlice";
 import playableAlbumCoverReducer from "./slices/PlayableAlbumCoverSlice";
 import { getDefaultMiddleware } from "@reduxjs/toolkit";
 
 const store = configureStore({
   reducer: {
     songSearch: songSearchReducer,
-    recommendedSongs: recommendedSongsReducer,
     playableAlbumCover: playableAlbumCoverReducer,
   },
   middleware: getDefaultMiddleware({
