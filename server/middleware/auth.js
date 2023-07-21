8a21312fba84d04d7d22522fd20b4f1ce4d21f18
Richard Chen <richard.chen@ualberta.ca>
2023-07-20 18:36:43 -0700

    Remove mui v4 & get POST user endpoint working with frontend

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -13,7 +13,7 @@ const checkJwt = jwt({
   }),
 
   // Validate the audience and the issuer
-  audience: process.env.AUTH0_AUDIENCE,
+  audience: process.env.REACT_APP_AUTH0_AUDIENCE,
   issuer: `https://${process.env.REACT_APP_AUTH0_DOMAIN}/`,
   algorithms: ["RS256"],
 });
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,33 +1,42 @@
 import { developerData } from "../assets/data/developerData";
 import AboutUsCard from "../components/AboutUsCard";
-import {Container} from "@mui/material";
-import {Typography} from "@material-ui/core";
+import { Container, Typography } from "@mui/material";
 import Box from "@mui/material/Box";
 
 export const About = () => {
-    return (
-        <Container maxWidth="lg">
-            <Typography variant="h4" align="center" style={{ margin: "25px" }}>
-                About Vibesphere
-            </Typography>
-            <Typography variant="body1" align="center">
-                Vibesphere is a Spotify song recommender application that is aimed at music lovers who want to discover new, but similar music to the songs they already like. It will store user inputted songs, retrieve results from the Spotify API, and recommend songs that will be delivered to our users. Users will be able to like songs, view history of previous songs inputted, then share these songs and the recommendations with their friends. Some additional functionality, given time constraints, will include: public playlists, a user profile, and recommendations based on entire playlists instead of individual songs.
-            </Typography>
-            <Typography variant="h4" align="center" style={{ margin: "25px" }}>
-                Meet the Developers
-            </Typography>
-            <Box sx={{
-                display:"flex",
-                flexDirection: 'row',
-                flexWrap: "wrap",
-                // width: "fit-content",
-                // marginLeft: "auto",
-                // marginRight: "auto"
-            }}>
-                {developerData.map((developer, index) => (
-                    <AboutUsCard developer={developer} key={index}/>
-                ))}
-            </Box>
-        </Container>
-    )
+  return (
+    <Container maxWidth="lg">
+      <Typography variant="h4" align="center" style={{ margin: "25px" }}>
+        About Vibesphere
+      </Typography>
+      <Typography variant="body1" align="center">
+        Vibesphere is a Spotify song recommender application that is aimed at
+        music lovers who want to discover new, but similar music to the songs
+        they already like. It will store user inputted songs, retrieve results
+        from the Spotify API, and recommend songs that will be delivered to our
+        users. Users will be able to like songs, view history of previous songs
+        inputted, then share these songs and the recommendations with their
+        friends. Some additional functionality, given time constraints, will
+        include: public playlists, a user profile, and recommendations based on
+        entire playlists instead of individual songs.
+      </Typography>
+      <Typography variant="h4" align="center" style={{ margin: "25px" }}>
+        Meet the Developers
+      </Typography>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          flexWrap: "wrap",
+          // width: "fit-content",
+          // marginLeft: "auto",
+          // marginRight: "auto"
+        }}
+      >
+        {developerData.map((developer, index) => (
+          <AboutUsCard developer={developer} key={index} />
+        ))}
+      </Box>
+    </Container>
+  );
 };
