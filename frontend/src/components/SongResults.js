55424fd6fd2b7652d27395301cb4b8f30c769eee
Richard Chen <richard.chen@ualberta.ca>
2023-08-07 14:07:39 -0700

    remove fragment

--- frontend/src/components/SongResults.js
+++ frontend/src/components/SongResults.js
@@ -232,72 +232,70 @@ export default function SongResults({
             </ListItemButton>
           ) : (
             songs.map((song) => (
-              <>
-                <div key={`div-${song.id}`}>
-                  <ListItemButton
-                    key={song.id}
-                    sx={{
-                      py: 0,
-                      minHeight: 75,
-                      color: "rgba(255,255,255,.8)",
-                    }}
-                    onClick={() => handleSongClick(song)}
-                  >
-                    <PlayableAlbumCover
-                      img={song.album.images[0].url}
-                      url={song.preview_url}
-                      size={50}
-                      mini={true}
-                      albumClickedCallback={handleAlbumClick}
-                    />
-                    <ListItemText
-                      sx={{
-                        minWidth: "35%",
-                        maxWidth: "35%",
-                        margin: "0px 10px",
-                        wordWrap: "break-word",
-                        overflow: "hidden",
-                      }}
-                      className="results-list-item-text-name"
-                      primary={song.name}
-                      secondary={song.artists[0].name}
-                      primaryTypographyProps={{
-                        fontSize: 14,
-                        fontWeight: "medium",
-                      }}
-                    />
-
-                    <ListItemText
-                      sx={{ margin: "0px 20px" }}
-                      className="results-list-item-text-album"
-                      primary={song.album.name}
-                      primaryTypographyProps={{
-                        fontSize: 14,
-                        fontWeight: "medium",
-                      }}
-                    />
-                    <LikeButton
-                      song={song}
-                      favoritedCallback={handleFavoritedCallback}
-                    />
-                    {user && (
-                      <IconButton
-                        aria-label="more"
-                        aria-controls="long-menu"
-                        aria-haspopup="true"
-                        onClick={(event) => handleClick(event, song)}
-                      >
-                        <MoreVertIcon />
-                      </IconButton>
-                    )}
-                  </ListItemButton>
-                  <SongPopupView
-                    song={popupSong}
-                    isDisplayed={displayPopup}
-                    handleClose={handlePopupClose}
+              <div key={`div-${song.id}`}>
+                <ListItemButton
+                  key={song.id}
+                  sx={{
+                    py: 0,
+                    minHeight: 75,
+                    color: "rgba(255,255,255,.8)",
+                  }}
+                  onClick={() => handleSongClick(song)}
+                >
+                  <PlayableAlbumCover
+                    img={song.album.images[0].url}
+                    url={song.preview_url}
+                    size={50}
+                    mini={true}
+                    albumClickedCallback={handleAlbumClick}
                   />
-                </div>
-              </>
+                  <ListItemText
+                    sx={{
+                      minWidth: "35%",
+                      maxWidth: "35%",
+                      margin: "0px 10px",
+                      wordWrap: "break-word",
+                      overflow: "hidden",
+                    }}
+                    className="results-list-item-text-name"
+                    primary={song.name}
+                    secondary={song.artists[0].name}
+                    primaryTypographyProps={{
+                      fontSize: 14,
+                      fontWeight: "medium",
+                    }}
+                  />
+
+                  <ListItemText
+                    sx={{ margin: "0px 20px" }}
+                    className="results-list-item-text-album"
+                    primary={song.album.name}
+                    primaryTypographyProps={{
+                      fontSize: 14,
+                      fontWeight: "medium",
+                    }}
+                  />
+                  <LikeButton
+                    song={song}
+                    favoritedCallback={handleFavoritedCallback}
+                  />
+                  {user && (
+                    <IconButton
+                      aria-label="more"
+                      aria-controls="long-menu"
+                      aria-haspopup="true"
+                      onClick={(event) => handleClick(event, song)}
+                    >
+                      <MoreVertIcon />
+                    </IconButton>
+                  )}
+                </ListItemButton>
+                <SongPopupView
+                  song={popupSong}
+                  isDisplayed={displayPopup}
+                  handleClose={handlePopupClose}
+                />
+              </div>
             ))
           )}
           {user && handleDelete && (
