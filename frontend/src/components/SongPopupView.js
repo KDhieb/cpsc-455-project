8a07944e995e8b3b529c6b7339e2ee97ebd6a833
KDhieb <khaliddhieb@gmail.com>
2023-08-07 12:38:22 -0700

    fix div child of tablebody console error

--- frontend/src/components/SongPopupView.js
+++ frontend/src/components/SongPopupView.js
@@ -1,22 +1,22 @@
-import React, { useState } from "react";
-import PropTypes from "prop-types";
-import { styled } from "@mui/material/styles";
-import Dialog from "@mui/material/Dialog";
-import DialogTitle from "@mui/material/DialogTitle";
-import DialogContent from "@mui/material/DialogContent";
-import DialogActions from "@mui/material/DialogActions";
-import IconButton from "@mui/material/IconButton";
-import CloseIcon from "@mui/icons-material/Close";
-import Typography from "@mui/material/Typography";
-import PlayableAlbumCover from "./PlayableAlbumCover";
-import { Share } from "@mui/icons-material";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSpotify } from "@fortawesome/free-brands-svg-icons";
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { styled } from '@mui/material/styles';
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
+import DialogActions from '@mui/material/DialogActions';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
+import Typography from '@mui/material/Typography';
+import PlayableAlbumCover from './PlayableAlbumCover';
+import { Share } from '@mui/icons-material';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSpotify } from '@fortawesome/free-brands-svg-icons';
 import { Snackbar } from '@mui/material';
-import LikeButton from "./LikeButton";
+import LikeButton from './LikeButton';
 
 const BootstrapDialog = styled(Dialog)(({ theme }) => ({
-  "& .MuiDialogActions-root": {
+  '& .MuiDialogActions-root': {
     padding: theme.spacing(1),
   },
 }));
@@ -32,7 +32,7 @@ function BootstrapDialogTitle(props) {
           aria-label='close'
           onClick={onClose}
           sx={{
-            position: "absolute",
+            position: 'absolute',
             right: 8,
             top: 8,
             color: (theme) => theme.palette.grey[500],
@@ -56,25 +56,25 @@ export default function SongPopupView({ isDisplayed, handleClose, song }) {
   if (!song) {
     return null;
   }
-  
-  const url = song.external_urls.spotify ?? `https://open.spotify.com/track/${song.id}`;
-  
+
+  const url =
+    song.external_urls.spotify ?? `https://open.spotify.com/track/${song.id}`;
+
   const handleSpotifyClick = () => {
-    window.open(url, "_blank", "noreferrer");
+    window.open(url, '_blank', 'noreferrer');
   };
 
   const handleShare = () => {
-    setOpenShare(true)
+    setOpenShare(true);
     navigator.clipboard.writeText(url);
   };
 
-
   return (
-    <div>
+    <>
       <BootstrapDialog
         fullWidth={true}
-        maxWidth={"xs"}
-        sx={{ margin: "auto" }}
+        maxWidth={'xs'}
+        sx={{ margin: 'auto' }}
         onClose={handleClose}
         aria-labelledby='customized-dialog-title'
         open={isDisplayed}
@@ -83,16 +83,17 @@ export default function SongPopupView({ isDisplayed, handleClose, song }) {
           id='customized-dialog-title'
           onClose={handleClose}
         >
-            <Typography sx={{ paddingBottom: "5px", fontSize: "1.5rem",
-    fontWeight: 500 }}>
-              {song.name ?? song.songName}
-            </Typography>
-            <Typography variant="p" sx={{ color: "lightgray" }}>
-              {song.artists[0].name}
-            </Typography>
+          <Typography
+            sx={{ paddingBottom: '5px', fontSize: '1.5rem', fontWeight: 500 }}
+          >
+            {song.name ?? song.songName}
+          </Typography>
+          <Typography variant='p' sx={{ color: 'lightgray' }}>
+            {song.artists[0].name}
+          </Typography>
         </BootstrapDialogTitle>
 
-        <DialogContent sx={{ margin: "auto" }}>
+        <DialogContent sx={{ margin: 'auto' }}>
           <PlayableAlbumCover
             img={song.album.images[0].url}
             url={song.preview_url}
@@ -103,19 +104,19 @@ export default function SongPopupView({ isDisplayed, handleClose, song }) {
 
         <DialogActions
           sx={{
-            display: "flex",
-            justifyContent: "space-evenly",
-            height: "100px",
+            display: 'flex',
+            justifyContent: 'space-evenly',
+            height: '100px',
           }}
         >
           <IconButton
             onClick={handleSpotifyClick}
-            sx={{ padding: "10px 10px" }}
+            sx={{ padding: '10px 10px' }}
           >
             <FontAwesomeIcon size='lg' icon={faSpotify} />
           </IconButton>
           <LikeButton song={song} />
-          <IconButton size={"large"} onClick={handleShare}>
+          <IconButton size={'large'} onClick={handleShare}>
             <Share fontSize="'large" />
           </IconButton>
           <Snackbar
@@ -123,15 +124,16 @@ export default function SongPopupView({ isDisplayed, handleClose, song }) {
             open={openShare}
             onClose={() => setOpenShare(false)}
             autoHideDuration={1500}
-            message="Copied Spotify link!"
+            message='Copied Spotify link!'
             ContentProps={{
               style: {
                 backgroundColor: 'black',
                 color: 'white',
-              }}}
+              },
+            }}
           />
         </DialogActions>
       </BootstrapDialog>
-    </div>
+    </>
   );
 }
